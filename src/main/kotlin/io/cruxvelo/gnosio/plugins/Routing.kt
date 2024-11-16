2bbc5da0913f0023554ee3dc5d132b5d8073af4a
damiankaplon <kaplon.damian99@gmail.com>
2024-11-16 14:53:26 +0100

    Fix packages

--- src/main/kotlin/io/cruxvelo/gnosio/plugins/Routing.kt
+++ src/main/kotlin/io/cruxvelo/gnosio/plugins/Routing.kt
@@ -1,15 +1,13 @@
-package io.crux
-
--velo.gnosio.plugins
+package io.cruxvelo.gnosio.plugins
 
 import io.ktor.server.application.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
 
 fun Application.configureRouting() {
-	routing {
-		get("/") {
-			call.respondText("Hello World!")
-		}
-	}
+    routing {
+        get("/") {
+            call.respondText("Hello World!")
+        }
+    }
 }
